84033335d1dbd07f24d88487663028256eddd017
Kyle Barnett <kbarnett23@gmail.com>
2019-11-02 10:57:30 -0600

    GET request working

---
+++ data/posts/posts-router.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+
+const db = require('../db');
+
+//POST METHODS
+router.post('/', (req, res) => {
+  const postInfo = req.body;
+  if (!postInfo.title || !postInfo.contents) {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+  } else {
+    db.insert(postInfo)
+      .then(data => {
+        res.status(201).json(data)
+      })
+      .catch(err => {
+        res.status(500).json({ error: "There was an error while saving the post to the database." })
+      })
+  }
+})
+
+router.post('/:id/comments', async (req, res) => {
+  const comments = { ...req.body, comment_id: req.params.id }
+  try {
+    const comment = await db.insertComment(comments)
+    res.status(201).json(comment)
+  } catch {
+    res.status(500).json({ error: "There was an error while saving the comment to the database." })
+  }
+})
+
+//GET METHODS
+router.get('/', (req, res) => {
+  const posts = req.body;
+  db.find(posts)
+    .then(post => {
+      res.status(200).send(post)
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The post information could not be retrieved." })
+    })
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const postsRouter = require('./data/posts/posts-router');
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/posts', postsRouter);
 
 server.listen(4000, () => {
   console.log('Listening on port 4000')
