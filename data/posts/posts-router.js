d721e79f410330d909e2ba88b5ee822583a28b4f
Kyle Barnett <kbarnett23@gmail.com>
2019-11-02 15:28:03 -0600

    PUT working

--- data/posts/posts-router.js
+++ data/posts/posts-router.js
@@ -71,11 +71,29 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+//PUT METHODS
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const post = req.body;
+  if (!post.title || !post.contents) {
+    res.status(400).json({ message: "The post with the specified ID does not exist." })
+  }
+  db.update(id, post)
+    .then(info => {
+      if (info) {
+        res.status(200).json(info)
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The post information could not be modified." })
+    })
+})
+
 //DELETE METHODS
 router.delete('/:id', (req, res) => {
   const id = req.params.id;
   db.remove(id)
-  .then(post => {
+    .then(post => {
       if (!post) {
         res.status(404).json({ error: "The post with the specified ID does not exist." })
       }
