e387f45320674adfe117a2e34282341bfb016fc4
Kyle Barnett <kbarnett23@gmail.com>
2019-11-07 19:26:50 -0700

    deployed

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const server = express();
 server.use(express.json());
 
 server.use('/api/posts', postsRouter);
-
+server.use('/', (req, res) => {
+  res.send(`<h1>Go to /api/posts for JSON data.</h1>`)
+})
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
