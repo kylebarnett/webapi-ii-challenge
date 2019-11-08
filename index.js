e63e5570997e90d1ab2e2b1cd91db06f02d8ff75
Kyle Barnett <kbarnett23@gmail.com>
2019-11-07 18:51:56 -0700

    moved more things

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const express = require('express');
 const postsRouter = require('./data/posts/posts-router');
 const server = express();
 
-const port = process.env.PORT || 5000;
 server.use(express.json());
 
 server.use('/api/posts', postsRouter);
 
 
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
